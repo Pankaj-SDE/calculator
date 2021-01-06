5ee7b622b62fa2d7fa824e0e46208cd62b64238a
Pankaj-SDE <pankaj.kumar@cleartax.in>
2021-01-07 00:13:26 +0530

    added calculator functionality

---
+++ src/main/java/SimpleCalculator.java
@@ -0,0 +1,6 @@
+public interface SimpleCalculator {
+    public int add(int a, int b);
+    public int sub(int a, int b);
+    public int multiply(int a, int b);
+    public int divide(int a, int b);
+}
---
+++ src/main/java/SimpleCalculatorImpl.java
@@ -0,0 +1,17 @@
+public class SimpleCalculatorImpl implements SimpleCalculator {
+    public int add(int a, int b) {
+        return 0;
+    }
+
+    public int sub(int a, int b) {
+        return 0;
+    }
+
+    public int multiply(int a, int b) {
+        return 0;
+    }
+
+    public int divide(int a, int b) {
+        return 0;
+    }
+}
